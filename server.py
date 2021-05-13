ff7e1c7b30dd1fe3b4295f01fba109af84558559
razevedo1994 <razevedo.contato@gmail.com>
2021-05-12 23:18:14 -0300

    create class BlogPost

--- server.py
+++ server.py
@@ -32,3 +32,12 @@ class User(db.Model):
     address = db.Column(db.String(200))
     phone = db.Column(db.String(50))
     posts = db.relationship("BlogPost")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.column(db.String(200))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
