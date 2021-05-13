bc4d6c78713194df0cfa2db82c27a8c27baba17a
razevedo1994 <razevedo.contato@gmail.com>
2021-05-12 22:45:28 -0300

    configure sqlite3 to enforce foreign key contraints

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from datetime import datetime
 from sqlite3 import Connection as SQLite3Connection
 
@@ -6,6 +9,15 @@ from sqlite3 import Connection as SQLite3Connection
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.file"
 app.config["SQL_TRACK_MODIFICATIONS"] = 0
+
+# Configure sqlite3 to enforce foreign key contraints
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys = ON;")
+        cursor.close()
+
 
 # Models
 class User(db.Model):
