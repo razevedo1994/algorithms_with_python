a219a4beaf7ab8d586aacbb5329393a17c2f486b
Rodrigo Azevedo da Silva <azevedo@MacBook-de-Rodrigo.local>
2021-10-16 14:39:33 -0300

    fix error

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -13,6 +13,11 @@ def binary_search(list, item):
             hight = mid - 1
 
         else:
-            low = mid - 1
+            low = mid + 1
 
     return None
+
+
+if __name__ == "__main__":
+    x = binary_search([1, 3, 5, 7, 9], 3)
+    print(x)
